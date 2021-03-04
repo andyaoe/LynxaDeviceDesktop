ec46667e12462be8b0b90fd642b40ffba1f274e9
Andy <misteraoe@gmail.com>
2021-03-04 06:47:47 -0800

    integrated protobuf

--- ProtoBufTest/Program.cs
+++ ProtoBufTest/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Protobuf;
+using System.IO;
 
 namespace ProtoBufTest
 {
@@ -10,6 +12,28 @@ namespace ProtoBufTest
     {
         static void Main(string[] args)
         {
+            NmeaRecord_100 nmea_record_100 = new NmeaRecord_100
+            {
+                Timestamp = 12345,
+                Latitude = 1.0f,
+                Longitude = 1.0f
+            };
+
+            using (var output = File.Create("nmea_serialized.dat"))
+            {
+                nmea_record_100.WriteTo(output);
+            }
+
+            using (var input = File.OpenRead("nmea_serialized.dat"))
+            {
+                nmea_record_100 = NmeaRecord_100.Parser.ParseFrom(input);
+            }
+
+            Console.WriteLine("Nmea Record Contents");
+            Console.WriteLine("TimeStamp=" + nmea_record_100.Timestamp);
+            Console.WriteLine("Latitude=" + nmea_record_100.Latitude);
+            Console.WriteLine("Longitude=" + nmea_record_100.Longitude);
+            Console.WriteLine("COMPLETE!");
         }
     }
 }
