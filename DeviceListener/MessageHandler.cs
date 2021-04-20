c80cf0c2310314befa97f2024f4b0481a8c2d87a
Andy <misteraoe@gmail.com>
2021-04-20 16:58:20 -0700

    implement new packet fields

--- DeviceListener/MessageHandler.cs
+++ DeviceListener/MessageHandler.cs
@@ -16,6 +16,8 @@ namespace Lynxa
     
     public class LynxaMessageInfo
     {
+        public byte modifier { get; set; }
+        public UInt32 deviceUid { get; set; }
         public UInt16 messageId { get; set; }
         public UInt16 totalMessageSize { get; set; }
         public UInt16 payloadSize { get; set; }
@@ -26,19 +28,16 @@ namespace Lynxa
     public class MessageHandler
     {
         public int _parserState = 0;
-        private byte _messageIdLsb;
-        private byte _messageIdMsb;
+        private byte _modifier;
+        private UInt32 _deviceUid;
         private UInt16 _messageId;
-        private byte _payloadSizeLsb;
-        private byte _payloadSizeMsb;
         private UInt16 _payloadSize;
-        private byte _checksumLsb;
-        private byte _checksumMsb;
         private UInt16 _checksum;
         private byte[] _packetBuffer = new byte[1024];
         private UInt16 _packetCounter = 0;
         private UInt16 _payloadCounter = 0;
         private UInt16 _checksumPayloadIndex = 0;
+        private UInt16 _payloadIndex = 0;
 
         private void ResetStateMachine()
         {
@@ -84,25 +83,47 @@ namespace Lynxa
                         reset_state_machine = true;
                     }
                     break;
-                case 2: //save the message id (MSB)
-                    _messageIdMsb = value;
+                case 2: //save the modifier
+                    _modifier = value;
                     _parserState++;
                     break;
-                case 3: //save the message id (LSB)
-                    _messageIdLsb = value;
-                    _messageId = (UInt16)((_messageIdMsb << 8) + _messageIdLsb);
+                case 3: //save the device UID (MSB)
+                    _deviceUid = (UInt32)(value << 24);
                     _parserState++;
                     break;
-                case 4: //save the payload size (MSB)
-                    _payloadSizeMsb = value;
+                case 4: //save the device UID
+                    _deviceUid |= (UInt32)(value << 16);
                     _parserState++;
                     break;
-                case 5: //save the payload size (LSB)
-                    _payloadSizeLsb = value;
-                    _payloadSize = (UInt16)((_payloadSizeMsb << 8) + _payloadSizeLsb);
+                case 5: //save the device UID
+                    _deviceUid |= (UInt32)(value << 8);
                     _parserState++;
                     break;
-                case 6: //save each byte of the payload
+                case 6: //save the device UID (LSB)
+                    _deviceUid |= (UInt32)(value << 0);
+                    _parserState++;
+                    //Console.WriteLine($"_deviceUid={_deviceUid}");
+                    break;
+                case 7: //save the message id (MSB)
+                    _messageId = (UInt16)(value << 8);
+                    _parserState++;
+                    break;
+                case 8: //save the message id (LSB)
+                    _messageId |= (UInt16)(value << 0);
+                    _parserState++;
+                    //Console.WriteLine($"_messageId={_messageId}");
+                    break;
+                case 9: //save the payload size (MSB)
+                    _payloadSize = (UInt16)(value << 8);
+                    _parserState++;
+                    break;
+                case 10: //save the payload size (LSB)
+                    _payloadSize |= (UInt16)(value << 0);
+                    _parserState++;
+                    _payloadIndex = _packetCounter;
+                    //Console.WriteLine($"_payloadSize={_payloadSize}");
+                    break;
+                case 11: //save each byte of the payload
                     _payloadCounter++;
                     if (_payloadCounter == _payloadSize)
                     {
@@ -110,28 +131,27 @@ namespace Lynxa
                         _parserState++;
                     }
                     break;
-                case 7: //save the checksum (MSB)
-                    _checksumMsb = value;
+                case 12: //save the checksum (MSB)
+                    _checksum = (UInt16)(value << 8);
                     _parserState++;
                     break;
-                case 8: //save the checksum (LSB)
-                    _checksumLsb = value;
-                    _checksum = (UInt16)((_checksumMsb << 8) + _checksumLsb);
+                case 13: //save the checksum (LSB)
+                    _checksum |= (UInt16)(value << 0);
                     _parserState++;
-                    break;
-                case 9: //verify checksum
-                    if (value == 0)
-                    {
-                        for (int i = 0; i < _checksumPayloadIndex; i++)
-                        {
-                            calculated_checksum += _packetBuffer[i];
-                        }
+                    //Console.WriteLine($"_checksum={_checksum}");
 
-                        if (_checksum == calculated_checksum)
-                        {
-                            //packet is valid
-                            full_packet_received = true;
-                        }
+                    //verify checksum
+                    for (int i = 0; i < _checksumPayloadIndex; i++)
+                    {
+                        calculated_checksum += _packetBuffer[i];
+                    }
+
+                    //Console.WriteLine($"calculated_checksum={calculated_checksum}");
+
+                    if (_checksum == calculated_checksum)
+                    {
+                        //packet is valid
+                        full_packet_received = true;
                     }
                     reset_state_machine = true;
                     break;
@@ -140,6 +160,8 @@ namespace Lynxa
             if (full_packet_received)
             {
                 lynxa_message_info = new LynxaMessageInfo();
+                lynxa_message_info.modifier = _modifier;
+                lynxa_message_info.deviceUid = _deviceUid;
                 lynxa_message_info.messageId = _messageId;
                 lynxa_message_info.payloadSize = _payloadSize;
                 lynxa_message_info.totalMessageSize = _packetCounter;
@@ -147,7 +169,7 @@ namespace Lynxa
                 lynxa_message_info.payloadBuffer = new byte[_payloadSize];
 
                 Array.Copy(_packetBuffer, 0, lynxa_message_info.messageBuffer, 0, _packetCounter);
-                Array.Copy(_packetBuffer, 6, lynxa_message_info.payloadBuffer, 0, _payloadSize);
+                Array.Copy(_packetBuffer, _payloadIndex, lynxa_message_info.payloadBuffer, 0, _payloadSize);
             }
 
             if (reset_state_machine)
