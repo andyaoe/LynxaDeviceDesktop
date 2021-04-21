2a4bbabbed0c0bc17bf7aa820b18447b1b909a36
Andy <misteraoe@gmail.com>
2021-04-20 22:25:27 -0700

    Add LynxaSerialPort and DeviceHost Application

---
+++ DeviceHost/LynxaSerialPort.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Lynxa
+{
+    public class LynxaSerialPort
+    {
+        SerialPort _serialPort;
+        string _portName;
+        int _baudRate;
+        MessageHandler _myMessageHandler = new MessageHandler();
+        //public event EventHandler<Type, object> LynxaPacketReceivedEvent;
+        public event EventHandler<object> LynxaPacketReceivedEvent;
+
+        public LynxaSerialPort(string portName, int baudRate)
+        {
+            _portName = portName;
+            _baudRate = baudRate;
+        }
+
+        public void Open()
+        {
+            _serialPort = new SerialPort(_portName, _baudRate, Parity.None, 8, StopBits.One);
+            _serialPort.Handshake = Handshake.None;
+            _serialPort.ReadTimeout = 1000;
+            _serialPort.WriteTimeout = 1000;
+            _serialPort.Open();
+
+            //start thread
+            Thread readThread = new Thread(Read);
+            readThread.Start();
+
+        }
+
+        public void Close()
+        {
+            _serialPort.Close();
+        }
+
+        public void Read()
+        {
+            while (_serialPort.IsOpen)
+            {
+                try
+                {
+                    int data = _serialPort.ReadByte();
+                    if (data != -1)
+                    {
+                        LynxaMessageInfo lynxa_message_info = _myMessageHandler.ParsePacket((byte)data);
+                        if (lynxa_message_info != null)
+                        {
+                            switch (lynxa_message_info.messageId)
+                            {
+                                case 100:
+                                    GnggaMessage_100 nmeaRecord_100 = GnggaMessage_100.Parser.ParseFrom(lynxa_message_info.payloadBuffer);
+                                    LynxaPacketReceivedEvent?.Invoke(this, nmeaRecord_100);
+                                    break;
+                                case 102:
+                                    WifiStationList_102 wifiStationList_102 = WifiStationList_102.Parser.ParseFrom(lynxa_message_info.payloadBuffer);
+                                    LynxaPacketReceivedEvent?.Invoke(this, wifiStationList_102);
+                                    break;
+                                case 103:
+                                    ModemParameters_103 modemParameters_103 = ModemParameters_103.Parser.ParseFrom(lynxa_message_info.payloadBuffer);
+                                    LynxaPacketReceivedEvent?.Invoke(this, modemParameters_103);
+                                    break;
+                            }
+                        }
+                    }
+                }
+                catch (TimeoutException te)
+                {
+                    //comes here if there was a timeout
+                }
+            }
+        }
+    }
+}
---
+++ DeviceHost/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using Lynxa;
+using System.IO;
+
+namespace DeviceListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LynxaSerialPort lynxaSerialPort = new LynxaSerialPort("COM2", 115200);
+            lynxaSerialPort.LynxaPacketReceivedEvent += LynxaSerialPort_LynxaPacketReceivedEvent;
+            lynxaSerialPort.Open();
+
+            while (true)
+            {
+                //exit if a key is pressed
+                if (Console.KeyAvailable)
+                {
+                    //save file and exit
+                    break;
+                }
+            }
+        }
+
+        private static void LynxaSerialPort_LynxaPacketReceivedEvent(object sender, object e)
+        {
+            Console.WriteLine($"Received Packet: { e.GetType() }");
+
+            switch (e)
+            {
+                case GnggaMessage_100 gnggaMessage:
+                    Console.WriteLine($"epochTime:{gnggaMessage.EpochTime}");
+                    Console.WriteLine($"LatitudeMinutes:{gnggaMessage.LatitudeMinutes}");
+                    Console.WriteLine($"LatitudeDegrees:{gnggaMessage.LatitudeDegrees}");
+                    Console.WriteLine($"LatitudeCardinalAscii:{gnggaMessage.LatitudeCardinalAscii}");
+                    Console.WriteLine($"longitudeMinutes:{gnggaMessage.LongitudeMinutes}");
+                    Console.WriteLine($"longitudeDegrees:{gnggaMessage.LongitudeDegrees}");
+                    Console.WriteLine($"longitudeCardinalAscii:{gnggaMessage.LongitudeCardinalAscii}");
+                    Console.WriteLine($"numberOfSatellitesInUse:{gnggaMessage.NumberOfSatellitesInUse}");
+                    break;
+                case WifiStationList_102 wifiStationList:
+                    Console.WriteLine($"epochTime:{wifiStationList.EpochTime}");
+                    Console.WriteLine($"Number of Wifi Stations:{wifiStationList.NumberStationsFound}");
+                    for (int i = 0; i < wifiStationList.NumberStationsFound; i++)
+                    {
+                        Console.Write($"BSSID:");
+                        Console.Write($"{wifiStationList.WifiStations[i].Bssid[0]}:");
+                        Console.Write($"{wifiStationList.WifiStations[i].Bssid[1]}:");
+                        Console.Write($"{wifiStationList.WifiStations[i].Bssid[2]}:");
+                        Console.Write($"{wifiStationList.WifiStations[i].Bssid[3]}:");
+                        Console.Write($"{wifiStationList.WifiStations[i].Bssid[4]}:");
+                        Console.Write($"{wifiStationList.WifiStations[i].Bssid[5]}\r\n");
+                        Console.WriteLine($"RSSI:{wifiStationList.WifiStations[i].Rssi}");
+                    }
+                    break;
+                case ModemParameters_103 modemParameters:
+                    Console.WriteLine("ModemParameters Received:");
+                    Console.WriteLine($"epochTime:{modemParameters.EpochTime}");
+                    Console.WriteLine($"CellId:{modemParameters.CellId}");
+                    Console.WriteLine($"PLMN:{modemParameters.Plmn}");
+                    Console.WriteLine($"TAC:{modemParameters.Tac}");
+                    break;
+            }
+        }
+    }
+}
